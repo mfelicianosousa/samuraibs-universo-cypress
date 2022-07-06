6e7745ad42082b8858982bfdf05556f0ff19e8e2
Marcelino Feliciano de Sousa <marcelino.feliciano@hotmail.com>
2022-07-06 09:48:22 -0400

    Solicitação de resgate de senha

--- cypress/support/components/toast/index.js
+++ cypress/support/components/toast/index.js
@@ -3,7 +3,7 @@ import { el } from './elements'
 class Toast {
 
     shouldHaveText(expectText) {
-        cy.get(el.toast)
+        cy.get(el.toast, {timeout:8000})
             .should('be.visible')
             .find('p')
             .should('have.text', expectText)
