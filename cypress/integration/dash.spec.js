a8f9b9080f2ba4ac2f8993953e51e8eb5a806a6d
Marcelino Feliciano de Sousa <marcelino.feliciano@hotmail.com>
2022-07-11 09:10:03 -0400

    Refatorando fixtures dash para Factories em Javascript

--- cypress/integration/dash.spec.js
+++ cypress/integration/dash.spec.js
@@ -1,45 +1,31 @@
 import loginPage from '../support/pages/login'
 import dashPage from '../support/pages/dash'
+import {customer,provider, appointment} from '../support/factories/dash'
 
 describe('dashboard', function(){
     context('quando o cliente faz um agendamento no app mobile',function(){
 
-        const data = {
-            customer:{
-                name: 'Nikki Sixx',
-                email: 'sixx@motleycrue.com',
-                password: 'pwd123',
-                is_provider: false
-            },
-            provider: {
-                name: 'Ramon Valdes',
-                email: 'ramon@television.com',
-                password: 'pwd123',
-                is_provider: true 
-            },
-            appointmentHour : '14:00'
-        }
-       
+           
 
         before(function(){
-            cy.postUser(data.provider)
-            cy.postUser(data.customer)
+            cy.postUser(provider)
+            cy.postUser(customer)
             //
-            cy.apiLogin(data.customer)
+            cy.apiLogin(customer)
             cy.log('token : '+Cypress.env('apiToken'))
-            cy.setProviderId( data.provider.email) 
-            cy.createAppointment(data.appointmentHour);  
+            cy.setProviderId( provider.email) 
+            cy.createAppointment(appointment.hour);  
         })
 
         it('o mesmo deve ser exibido no dashboard', function(){
             // cy.log('O id do Ramon é '+Cypress.env('providerId'))
             loginPage.go()
-            loginPage.form(data.provider)
+            loginPage.form(provider)
             loginPage.submit()
             dashPage.calendarShoudBeVisible()
             const day = Cypress.env('appointmentDay')
             dashPage.selectDay(day)   
-            dashPage.appointmentShouldBe(data.customer, data.appointmentHour) 
+            dashPage.appointmentShouldBe(customer, appointment.hour) 
           
         })
     })
